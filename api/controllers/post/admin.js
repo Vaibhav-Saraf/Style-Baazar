6858a664f8be44f753fa509de230020669bccb79
Vaibhav-Saraf <vaibhavsaraf73@gmail.com>
2021-11-27 21:44:13 +0530

    Final Push V1.0.0

---
+++ api/controllers/post/admin.js
@@ -0,0 +1,9 @@
+const PostsController = require("../PostsController")
+
+module.exports = async function(req, res) {
+    
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type,Accept");
+    res.view('pages/adminLogin')
+
+}
---
+++ api/models/Offer.js
@@ -0,0 +1,17 @@
+module.exports = {
+    attributes:{
+        // division:{
+        //     type: 'string',
+        //     required: false
+        // },
+        ot:{
+            type: 'string',
+            required: false
+        },
+        od:{
+            type: 'string',
+            required: false
+        },
+    }
+
+};
